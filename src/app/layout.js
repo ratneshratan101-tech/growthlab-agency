de0cc698d140f8a944dcdd43aae79ddcea28dadc
ratneshratan101-tech <ratneshratan101@gmail.com>
2026-06-05 01:48:53 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import './globals.css'
+import '../styles/globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/layout/Navbar'
 import Footer from '@/components/layout/Footer'
